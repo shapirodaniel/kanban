0c5b52e6445e3add696c1f5d1dbeeff3b1383957
shapirodaniel <shapirodanieladam@gmail.com>
2021-05-19 13:12:20 -0500

    fix: modify project title and fix routers

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -11,7 +11,7 @@ const UserOrganization = db.define('user_organization', {
 
   // status will be used to determine whether a user_organization
   // is an INVITE (with status: pending)
-  status: Sequelize.ENUM('pending', 'active'),
+  status: Sequelize.ENUM('pending', 'active')
 })
 
 // associations
@@ -24,13 +24,13 @@ Project.belongsTo(Organization)
 Organization.belongsToMany(User, {through: UserOrganization})
 User.belongsToMany(Organization, {through: UserOrganization})
 
-Task.belongsToMany(User)
-User.belongsToMany(Task)
+Task.belongsToMany(User, {through: 'UserTask'})
+User.belongsToMany(Task, {through: 'UserTask'})
 
 module.exports = {
   User,
   Organization,
   Project,
   Task,
-  UserOrganization,
+  UserOrganization
 }
